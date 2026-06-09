ee40ddf580501f38703d287dd5eb2fdb9b71a71b
Esha Rameel <esharameel18@gmail.com>
2026-06-10 00:37:33 +0530

    Add testbench for D flip-flop module

---
+++ day 2/d flip flop/testbench/d_ff_tb.v
@@ -0,0 +1,24 @@
+module d_ff_tb();
+reg d_tb,rst_tb,clk_tb;
+wire q_tb,qbar_tb;
+
+d_ff dut(d_tb,rst_tb,clk_tb,q_tb,qbar_tb);
+initial 
+begin
+ {d_tb,rst_tb,clk_tb}=0;
+end 
+always#5 clk_tb=~clk_tb;
+initial 
+begin 
+ rst_tb=1; 
+ #10;
+ rst_tb=0;
+ d_tb=0;
+ #10;
+ d_tb=1;
+ #10;
+ d_tb=0;
+ #10;
+ d_tb=1;
+end 
+endmodule
